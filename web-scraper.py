ba45f7f0dc2919e26e18ddb47a587594d17bb51d
lhoangphuong <49200808+lhoangphuong@users.noreply.github.com>
2021-04-13 23:41:39 +0700

    Update web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -2,17 +2,26 @@ import pandas as pd
 import os
 import webbrowser
 
-table_MN = pd.read_html('https://en.wikipedia.org/wiki/Minnesota')
+# df = data frame
+url = str(
+    input('Enter website url: ')
+    or 'https://en.wikipedia.org/wiki/Minnesota')
 
-df_list = pd.read_html('https://en.wikipedia.org/wiki/Minnesota', match='Election results from statewide races')
-df_list[0].to_csv('Election_result.csv',index=False)
+dfs = pd.read_html(url)
+print(f'There are {len(dfs)} tables in this website')
 
-a = pd.read_csv("Election_result.csv")
+match = str(
+    input('Enter an matching string of the table: ')
+    or 'Election results from statewide races')
+
+df_list = pd.read_html('https://en.wikipedia.org/wiki/Minnesota', match=f'{match}')
+df_list[0].to_csv('result.csv',index=False)
+
+a = pd.read_csv("result.csv")
 html_file = a.to_html(index=False)
 
-Html_file= open("test.html","w")
+Html_file= open("result.html","w")
 Html_file.write(html_file)
 Html_file.close()
 
-url = "file:///D:/test/test.html"
-webbrowser.open(url,new=2)
+webbrowser.open('file://' + os.path.realpath("result.html"), new=2)
